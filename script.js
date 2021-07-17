66f38bdf1b5003064ad21e8d18a5a62b46b6880a
SteveNoyes <stevenmnoyes.af@gmail.com>
2021-07-17 15:27:25 -0500

    social

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var frmvalidator  = new Validator("contactform");
 frmvalidator.addValidation("name","req","Please provide your name.");
 frmvalidator.addValidation("email","req","Please provide your email.");
 frmvalidator.addValidation("email","email", "Please enter a valid email address.");
+
+// MENU TOGGLE
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
